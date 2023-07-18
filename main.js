96c91601002511bc711902429703bc77dd7c227d
Tarik Mey <tarik.mey@gmail.com>
2023-07-18 23:01:05 +0200

    adding the task and task content to the task list. The user input is collected and added to the task list.

--- main.js
+++ main.js
@@ -6,6 +6,29 @@ window.addEventListener("load", () => {
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        console.log("submit form " + e.input);
+        // fetching the task from the form input line
+        const task = input.value;
+
+        if(!task) {
+            // alert the user to type a task name before submitting
+            alert("Please fill out the task");
+            return;
+        }
+
+        // creating the DOM elements to add to the page
+        // creating the task
+        const task_el = document.createElement("div");
+        task_el.classList.add("task");
+
+        // creating the task content
+        const task_content_el = document.createElement("div");
+        task_content_el.classList.add("content");
+        task_content_el.innerText = task;
+
+        // appending the content to the related task
+        task_el.appendChild(task_content_el);
+
+        // appending the task to the task list
+        list_el.appendChild(task_el);
     })
 })
