473cd3a5c1a31844699bab067151999a515e0135
Tarik Mey <tarik.mey@gmail.com>
2023-07-18 23:13:39 +0200

    user can input a task and it gets loaded to the list with its action buttons

--- main.js
+++ main.js
@@ -23,12 +23,39 @@ window.addEventListener("load", () => {
         // creating the task content
         const task_content_el = document.createElement("div");
         task_content_el.classList.add("content");
-        task_content_el.innerText = task;
 
         // appending the content to the related task
         task_el.appendChild(task_content_el);
 
+        // creating the task as an DOM element
+        const task_input_el = document.createElement("input");
+        task_input_el.classList.add("text");
+        task_input_el.type = "text";
+        task_input_el.value = task;
+        task_input_el.setAttribute("readonly", "readonly");
+
+        task_content_el.appendChild(task_input_el);
+
+        // creating the task action buttons
+        const task_actions_el = document.createElement("div");
+        task_actions_el.classList.add("actions");
+
+        const task_edit_el = document.createElement("button");
+        task_edit_el.classList.add("edit");
+        task_edit_el.innerHTML = "Edit";
+
+        const task_delete_el = document.createElement("button");
+        task_delete_el.classList.add("delete");
+        task_delete_el.innerHTML ="Delete";
+
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+
+        task_el.appendChild(task_actions_el);
+
         // appending the task to the task list
         list_el.appendChild(task_el);
+
+        input.value = "";
     })
 })
