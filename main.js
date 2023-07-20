0cea34b4a689225b24835ea02138df8303388ac2
Tarik Mey <tarik.mey@gmail.com>
2023-07-20 17:43:19 +0200

    adding edit button functionality

--- main.js
+++ main.js
@@ -93,7 +93,17 @@ function DisplayTodos() {
             DisplayTodos();
         })
 
-        
+        editBtn.addEventListener("click", (e) => {
+            const input = content.querySelector("input");
+            input.removeAttribute("readonly");
+            input.focus();
+            input.addEventListener("blur", (e) => {
+                input.setAttribute("readonly", true);
+                todo.content = e.target.value;
+                localStorage.setItem("todos", JSON.stringify(todos));
+                DisplayTodos();
+            })
+        })
     })
 }
 
